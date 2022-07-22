d9cba1b4d4d03339615d8ef252bc66e5340e94c2
allhailthethanos <55079966+allhailthethanos@users.noreply.github.com>
2022-07-22 15:42:41 +0530

    Testing changes

--- lib/country_state_city_picker.dart
+++ lib/country_state_city_picker.dart
@@ -7,6 +7,76 @@ import 'package:flutter/services.dart' show rootBundle;
 
 import 'model/select_status_model.dart' as StatusModel;
 
+class EditProfileDropdown extends StatelessWidget {
+  final String label;
+  final Widget? suffixIcon;
+  final String? initialValue;
+  final bool? readOnly;
+  final bool? enabled;
+  final VoidCallback? onTap;
+  final Function(String)? onChanged;
+  final TextEditingController? controller;
+  final DropdownButton<String>? child;
+  const EditProfileDropdown({
+    Key? key,
+    required this.label,
+    this.initialValue,
+    this.readOnly,
+    this.enabled,
+    this.onTap,
+    this.controller,
+    this.onChanged,
+    this.suffixIcon,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 14,
+            color: Color(0xFF7985A0),
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        InkWell(
+          onTap: onTap,
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color(0xFFD9DFEB),
+              ),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: IgnorePointer(
+                    ignoring: onTap != null,
+                    child: child,
+                  ),
+                ),
+                if (suffixIcon != null)
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: suffixIcon!,
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class SelectState extends StatefulWidget {
   final ValueChanged<String> onCountryChanged;
   final ValueChanged<String> onStateChanged;
@@ -160,32 +230,33 @@ class _SelectStateState extends State<SelectState> {
       children: <Widget>[
         InputDecorator(
           decoration: widget.decoration,
-          child: DropdownButtonHideUnderline(
+          child: EditProfileDropdown(
+              label: "Country",
               child: DropdownButton<String>(
-            dropdownColor: widget.dropdownColor,
-            isExpanded: true,
-            items: _country.map((String dropDownStringItem) {
-              return DropdownMenuItem<String>(
-                value: dropDownStringItem,
-                child: Row(
-                  children: [
-                    Flexible(
-                      child: Text(
-                        dropDownStringItem,
-                        style: widget.style,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-            // onTap: ,
-            onChanged: (value) => _onSelectedCountry(value!),
-            onTap: widget.onCountryTap,
-            // onChanged: (value) => _onSelectedCountry(value!),
-            value: _selectedCountry,
-          )),
+                dropdownColor: widget.dropdownColor,
+                isExpanded: true,
+                items: _country.map((String dropDownStringItem) {
+                  return DropdownMenuItem<String>(
+                    value: dropDownStringItem,
+                    child: Row(
+                      children: [
+                        Flexible(
+                          child: Text(
+                            dropDownStringItem,
+                            style: widget.style,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }).toList(),
+                // onTap: ,
+                onChanged: (value) => _onSelectedCountry(value!),
+                onTap: widget.onCountryTap,
+                // onChanged: (value) => _onSelectedCountry(value!),
+                value: _selectedCountry,
+              )),
         ),
         SizedBox(
           height: widget.spacing,
